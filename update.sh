1129c38e7113c6addbba92fc92b3a235d5e825d0
Fede <fedebosi04@gmail.com>
2020-05-16 13:43:13 +0200

    Fix update script

--- update.sh
+++ update.sh
@@ -6,6 +6,9 @@ git pull;
 echo "Controllo modifiche...";
 git status -uno;
 echo "Controllo eseguito!";
+echo "Aggiunta permesso eseguibile...";
+chmod +x ./update.sh
+echo "Fatto."
 md5=md5sum ./update.sh;
 sha1=sha1sum ./update.sh;
 echo "MD5: ${md5}";
