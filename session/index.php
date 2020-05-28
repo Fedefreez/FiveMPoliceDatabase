0c4be15eac3d7e73ca7996cdf8855178ce6ddaba
Fede <fedebosi04@gmail.com>
2020-05-28 19:56:31 +0200

    Date format in citizinfo is now Italian

--- session/index.php
+++ session/index.php
@@ -268,7 +268,7 @@
       var date_input=$('input[name="date"]'); //our date input has the name "date"
   		var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
   		date_input.datepicker({
-  			format: 'yyyy-mm-dd',
+  			format: 'dd-mm-yyyy',
   			container: container,
   			todayHighlight: true,
   			autoclose: true,
